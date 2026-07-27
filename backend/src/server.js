d3211479cf3c9eb0cfba77d44f1c7d04f3222450
sayanta mondal <sayantamondalrup@gmail.com>
2026-07-27 23:59:23 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+import express from "express"
+import dotenv from "dotenv"
+import { initDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+import transactionsRoute from "./routes/transactionsRoute.js"
+import job from "./config/cron.js";
+dotenv.config();
+
+const app=express();
+
+if(process.env.NODE_ENV === "production") job.start();
+
+//middleware
+app.use(rateLimiter);
+app.use(express.json())
+
+const PORT = process.env.PORT || 5201;
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({status: "ok"});
+});
+
+app.use("/api/transactions",transactionsRoute );
+
+initDB().then(()=>{
+    app.listen(PORT,()=>{
+    console.log("server is up and running", PORT);
+})
+})
+
+
+
+
+
+
